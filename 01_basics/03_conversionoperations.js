9a048ff32abdb8e6a4574d686153f1dd7e8e3bbc
198Udit <115370849+198Udit@users.noreply.github.com>
2025-09-02 11:59:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,28 @@
+// let score = undefined;
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber)); 
+// console.log(valueInNumber);
+
+
+//"33" = 33
+//"33" => NaN
+//true => 1;    false => 0
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+ // 0 => false;  1 => true;  
+ //"" => false 
+ // "udit" => true
+
+
+ let someNumber = 48;
+  
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log(typeof(stringNumber));
